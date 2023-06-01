71860075e4ef0f256ae20e6ca3985a6cce1fca49
changyu chen <chen.changyu@ruc.edu.cn>
2023-06-01 20:14:09 +0800

    f

--- config/ArgumentClass/ModelArguments.py
+++ config/ArgumentClass/ModelArguments.py
@@ -25,48 +25,6 @@ class BaseArguments:
         #return {"smile":"^_^"}
         return {}
 
-@register_argumentclass("model_seq2label")
-@dataclass
-class Seq2LabelArguments(BaseArguments):
-    auto_model = AutoModelForSequenceClassification
-    num_labels: int = field(default=1,metadata={"help":"num class for multiclass classfication"})
-    num_tasks:  int = field(default=1,metadata={"help":"num task for multitask classification"})
-    default_task: int = field(default=0,metadata={"help":"default task id"})
-    cls_mode: str = field(default="multi_task",metadata={"help":"multi_task or multi_class"})
-    loss_rce_beta: float = field(default=0.0, metadata= {"help":"hyper weight for ce"})
-    problem_type: str = field(default=None, metadata= {"help": "None (default)|regression|single_label_classification|multi_label_classification; default will try to infer problem type from labels"})
-
-@register_argumentclass("model_seq2label_unilm")
-@dataclass
-class UniLMSeq2Label(BaseArguments):
-    rel_pos_type: int = field(default=0,metadata={"help":"The rel postion type, 0 for disable"})
-    rel_pos_bins: int = field(default=0,metadata={"help":"The config for rel_pos_type"})
-    max_rel_pos: int = field(default=0,metadata={"help":"Max Relative position"})
-    num_labels: int = field(default=1,metadata={"help":"num class for multiclass classfication"})
-    fast_qkv: bool = field(default=False,metadata={"help":"Don't know"})
-    classifier_dropout: float = field(default=0.1,metadata={"help":"dropout for last layer"})
-
-@register_argumentclass("model_singletwinbert_classificationx")
-@dataclass
-class SingleTwinBertClassificationXArguments(BaseArguments):
-    num_labels: int = field(default=2,metadata={"help":"num class for multiclass classfication"})
-    embed_dim: int = field(default=32,metadata={"help":"output embedding dimension size"})
-    temperature: float = field(default=1.0,metadata={"help":"temperature in softmax"})
-    freeze: bool = field(default=True,metadata={"help":"freeze encoder weight"})
-    num_layers: int = field(default=None,metadata={"help":"Num of layers to use, default None will use all layers"})
-    tanh_activation: bool = field(default=False,metadata={"help":"apply tanh activation for embedding"})
-
-@register_argumentclass("model_twinbert_classificationx")
-@dataclass
-class TwinBertClassificationXArguments(BaseArguments):
-    temperature: float = field(default=1.0,metadata={"help":"temperature in softmax"})
-    num_layers: int = field(default=None,metadata={"help":"Num of layers to use, default None will use all layers"})
-    downscale_pooler: bool = field(default=True,metadata={"help":"Downscale BERT hidden representation to smaller dim before classification"})
-    post_xing_layers: int = field(default=0,metadata={"help":"if > 0, number of hidden layers after crossing layer; defaul=0 no hidden layer"})
-    post_xing_dim: int = field(default=192,metadata={"help":"dimension of hiddem layer"})
-    load_xing_params: bool = field(default=True,metadata={"help":"try to load parameters of crossing layer from ckpt"})
-
-
 
 
 
@@ -120,57 +78,4 @@ class Seq2SeqArguments(BaseArguments):
     nmask_next_comma: bool = field(default=True)
     fix_mask: bool=field(default=True)
     v2_0401: bool=field(default=False)
-@register_argumentclass("model_seq2tag")
-@dataclass
-class Seq2TagArguments(BaseArguments):
-    last_layer: str = field(default="fwd",metadata={"help":"Last layer type, support fwd, transformer"})
-    num_labels: int = field(default=2,metadata={"help":"num tags"})
-
-@register_argumentclass("model_pretrain")
-@dataclass
-class PretrainArguments(BaseArguments):
-    mlm_prob: float = field(default=0.15,metadata={"help":"Mask ratio"})
-
-@register_argumentclass("model_guided_seq2seq")
-@dataclass
-class GuidedSeq2SeqArguments(BaseArguments):
-    latent_class: int = field(default=4,metadata={"help":"Set Latent Class"})
-    selected_class: int = field(default=2,metadata={"help":"Set predict Latent Class count"})
-    guided_mode: str = field(default="enc_dec",metadata={"help":"Added latent signal to enc, enc_dec or dec"})
-    kl_loss_weight: float = field(default="0.0",metadata={"help":"Weight of KL loss"})
-    cls_loss_weight: float = field(default="0.0",metadata={"help":"Weight of CLS loss"})
-    em: str = field(default="hard",metadata={"help":"hard for hard-em, soft for soft-em"})
-    max_length: int = field(default=20,metadata={"help":"Longest sequence to be generated"})
-    min_length: int = field(default=5, metadata={"help":"Shortest sequence to be generated"})
-    num_beams:  int = field(default=1, metadata={"help":"beam size"})
-    repetition_penalty: float = field(default=1.0,metadata={"help":"factor to supress repetition"})
-    length_penalty:     float = field(default=1.0,metadata={"help":"factor to penalize short result"})
-    num_return_sequences: int = field(default=1,metadata={"help":"number of return sequences"})
-
-@register_argumentclass("model_selector_seq2seq")
-@dataclass
-class SelectorSeq2SeqArguments(BaseArguments):
-    sample_pos: int = field(default=1, metadata={"help": "The sample position. 0: before enc; 1: after enc"})
-    sample_num: int = field(default=2,metadata={"help":"The number of sentence smapled"})
-    sample_loss_weight: float = field(default="0.0",metadata={"help":"Weight of loss of sample classifier"})
-    latent_num: int = field(default=1,metadata={"help":"The number of latent variables"})
-    latent_loss_weight: float = field(default="0.0",metadata={"help":"Weight of loss of latent classifier"})
-    latent_enc_fusion: str = field(default="concat",metadata={"help":"The way to fusion the inputs emb and labels emb"})
-    max_length: int = field(default=20,metadata={"help":"Longest sequence to be generated"})
-    min_length: int = field(default=1, metadata={"help":"Shortest sequence to be generated"})
-    num_beams:  int = field(default=1, metadata={"help":"beam size"})
-    repetition_penalty: float = field(default=1.0,metadata={"help":"factor to supress repetition"})
-    length_penalty:     float = field(default=1.0,metadata={"help":"factor to penalize short result"})
-    num_return_sequences: int = field(default=1,metadata={"help":"number of return sequences"})
-    no_repeat_ngram_size: int = field(default=0,metadata={"help":"set no repeat ngram"})
-    early_stopping: bool = field(default=True,metadata={"help":"early stop for decoding"})
-
-@register_argumentclass("model_multi_task_opt_seq2seq")
-@dataclass
-class MultiTaskOptArguments(Seq2SeqArguments):
-    use_history_weight: bool = field(default=False, metadata={"help": "if task_weight is based on previous history or only current one"})
-    rank_alpha: float = field(default=0.2, metadata={"help": "hyperparameter for ranking sampler instances"})
-    task_scoring_method: str = field(default="batch_task_dist", metadata={"help": "either contant, batch_task_dist or task_loss"})
-    dist_alpha: float = field(default=1, metadata={"help": "adjust task distribution weight"})
-    loss_alpha: float = field(default=0., metadata={"help": "adjust loss contribution to sampling weight"})
 
--- models/t5.py
+++ models/t5.py
@@ -235,50 +235,6 @@ except ImportError:
         def forward(self, input):
             return input
 
-class ValueHead(nn.Module):
-    """The ValueHead class implements a head for GPT2 that returns a scalar for each output token."""
-    def __init__(self, config):
-        super().__init__()
-        self.detach_head = False
-        self.summary_type = config.summary_type if hasattr(config, "summary_type") else "last"
-        if self.summary_type == "attn":
-            raise NotImplementedError
-
-        # self.summary = Identity()
-        # if hasattr(config, "summary_use_proj") and config.summary_use_proj:
-        #     if hasattr(config, "summary_proj_to_labels") and config.summary_proj_to_labels and config.num_labels > 0:
-        #         num_classes = config.num_labels
-        #     else:
-        #         num_classes = config.hidden_size
-        num_classes = 1
-        self.summary = nn.Linear(config.hidden_size, num_classes)
-
-        self.activation = Identity()
-        # if hasattr(config, "summary_activation") and config.summary_activation == "tanh":
-        #     self.activation = nn.Tanh()
-
-        # self.first_dropout = Identity()
-        # if hasattr(config, "summary_first_dropout") and config.summary_first_dropout > 0:
-        #     self.first_dropout = nn.Dropout(config.summary_first_dropout)
-
-        self.first_dropout = nn.Dropout(0.1)
-        self.last_dropout = Identity()
-        # if hasattr(config, "summary_last_dropout") and config.summary_last_dropout > 0:
-        #     self.last_dropout = nn.Dropout(config.summary_last_dropout)
-
-        self.flatten = nn.Flatten()
-
-    def forward(self, hidden_states, cls_index=None):
-        if self.detach_head:
-            output = hidden_states.detach()
-        else:
-            output = hidden_states
-        output = self.first_dropout(output)
-        output = self.summary(output)
-        output = self.activation(output)
-        output = self.last_dropout(output)
-
-        return output
 
 
 @replace(T5ForConditionalGeneration)
@@ -290,8 +246,6 @@ class T5ForConditionalGeneration(T5ForConditionalGeneration):
 
         try:
 
-            if self.config.ppo:
-                self.v_head = ValueHead(config)
 
             self.mask_input = config.mask_input
             self.mask_rate = config.mask_rate
